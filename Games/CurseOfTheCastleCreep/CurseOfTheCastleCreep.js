50d83d7aaa41e047910fd13b6fa683b9673fb62e
Lovro Selic <lovro.selic@gmail.com>
2025-01-28 16:34:37 +0100

    1.01.02 added textures

--- Games/CurseOfTheCastleCreep/CurseOfTheCastleCreep.js
+++ Games/CurseOfTheCastleCreep/CurseOfTheCastleCreep.js
@@ -113,7 +113,7 @@ const INI = {
     COMPLAIN_TIMEOUT: 400,
 };
 const PRG = {
-    VERSION: "1.01.01",
+    VERSION: "1.01.02",
     NAME: "The Curse Of The Castle Creep",
     YEAR: "2023, 2024",
     SG: "CCC",
